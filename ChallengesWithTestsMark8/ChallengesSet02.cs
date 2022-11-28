3a42b0cceb2d79011fb667824d7e35e8d2498f30
earl starks <earlstarks@rocketmail.com>
2022-11-28 15:15:19 -0600

    wk two done

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Threading;
 
 namespace ChallengesWithTestsMark8
@@ -13,77 +14,87 @@ namespace ChallengesWithTestsMark8
         }
 
         public bool CountOfElementsIsEven(string[] vals)
-
-
-        string CountOfElementsIsEven = vals[0]; // right here vals tell coder i have 0 # of arrays
-        
-        if ( CountOfElementsIsEven % 2 = 0);// right here i set what is going to happen and return bool t/f
-        
         {
-            return true;
-        }
-        else 
-        {
-            return false;
+            var count = vals.Length;
+
+            return count % 2 == 0;
         }
 
         public bool IsNumberEven(int number)// do i have to define the number first? 
         {
-            bool IsNumberEven = false;
+            return number % 2 == 0;
         }
         
 
         public bool IsNumberOdd(int num)// or if number1 % number2 != 0 then true, else, false?
         {
-            bool IsNumberOdd = false;   
+            return num % 2 != 0;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)// math. add min + max= sum?
         {
-            Math.Min + int.MaxValue = Sum;
+            var min = numbers.Min();
+            var max = numbers.Max();
+
+            return min + max;
         }
 
         public int GetLengthOfShortestString(string str1, string str2)// phrase. length ??googled this answer.
         {
-            name = GetLengthOfShortestString;
-            string minValue = name[0];
-            foreach (string name in names)
-            {
-                if (name.Length < minValue.Length)
-                {
-                    minValue = name;
-                }
-            }
+            var str1Length = str1.Length;
+            var str2Length = str2.Length;
 
-            Console.WriteLine(minValue);
+            return str1Length < str2Length ? str1Length : str2Length;
         }
         
         public int Sum(int[] numbers)// simply math.add int + int = sum????
         {
-            throw new NotImplementedException();
+            var sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
         }
 
         public int SumEvens(int[] numbers) // no idea - I googled
         {
-            int sum = addNumbers();
+            //return numbers
+            //    .Where(number => number % 2 == 0)
+            //    .Sum();
+
+            var sum = 0;
+            foreach (var number in numbers)
+            {
+                if (number % 2 == 0) 
+                {
+                    sum += number;
+                }
+            }
+
+            return sum;
         }
 
         public bool IsSumOdd(List<int> numbers) // my best guess
         {
-            int sum = addNumbers();
-        }
-        if (int sum % 2 == 0);
-        {
-        return false;
-        {
-        else
-        {
-        return true;
+            var sum = numbers.Sum();
+
+            return sum % 2 != 0;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)// got help from google
         {
-            return n / 2; 
+            var count = 0;
+            for (var i = 0; i < number; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
     }
 }
